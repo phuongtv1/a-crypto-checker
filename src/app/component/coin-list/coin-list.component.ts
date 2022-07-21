29894f0c6769dce715d7874abe2aa8c8a7868780
phuongtv <phuongtv@vissoft.vn>
2022-07-21 09:28:43 +0700

    Updating

--- src/app/component/coin-list/coin-list.component.ts
+++ src/app/component/coin-list/coin-list.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/service/api.service';
+import { CurrencyService } from 'src/app/service/currency.service';
 @Component({
   selector: 'app-coin-list',
   templateUrl: './coin-list.component.html',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoinListComponent implements OnInit {
 
-  constructor() { }
+  bannerData: any = [];
+  currency : string = "VND"
+  dataSource!: MatTableDataSource<any>;
+  displayedColumns: string[] = ['symbol', 'current_price', 'price_change_percentage_24h', 'market_cap'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(private api: ApiService, private router : Router, private currencyService : CurrencyService) { }
 
   ngOnInit(): void {
+    this.getAllData();
+    this.getBannerData();
+    this.currencyService.getCurrency()
+    .subscribe(val=>{
+      this.currency = val;
+      this.getAllData();
+      this.getBannerData();
+    })
+  }
+  getBannerData() {
+    this.api.getTrendingCurrency(this.currency)
+      .subscribe(res => {
+        console.log(res);
+        this.bannerData = res;
+      })
+  }
+  getAllData() {
+    this.api.getCurrency(this.currency)
+      .subscribe(res => {
+        console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      })
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  gotoDetails(row: any) {
+    this.router.navigate(['coin-detail',row.id])
   }
 
 }
